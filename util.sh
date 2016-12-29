da20b2ae648a0705dcbfb17856fcec6a0ae80327
Matt Blair <me@matthewblair.net>
2016-12-28 22:50:26 -0500

    shfmt

--- util.sh
+++ util.sh
@@ -3,21 +3,21 @@
 set -xueo pipefail
 
 freshen() {
-  go get -u golang.org/x/tools/cmd/goimports
-  go get -u github.com/mvdan/sh/cmd/shfmt
-  go get -u github.com/shurcooL/markdownfmt
-  go get -u github.com/govend/govend
-  go get -u github.com/alecthomas/gometalinter
-  gometalinter --install --update
+	go get -u golang.org/x/tools/cmd/goimports
+	go get -u github.com/mvdan/sh/cmd/shfmt
+	go get -u github.com/shurcooL/markdownfmt
+	go get -u github.com/govend/govend
+	go get -u github.com/alecthomas/gometalinter
+	gometalinter --install --update
 }
 
 if [[ "$#" -gt 0 ]]; then
 	while [[ "$#" -gt 0 ]]; do
 		case "$1" in
-      --freshen)
-      freshen
-      shift;
-      ;;
-    esac
-  done
+			--freshen)
+				freshen
+				shift
+				;;
+		esac
+	done
 fi
