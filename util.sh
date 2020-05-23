ae5815276bceaa38951895975edd3fa3cb1b4a7c
Matt Blair <me@matthewblair.net>
2020-05-22 17:48:05 -0700

    more gardening

--- util.sh
+++ util.sh
@@ -6,9 +6,6 @@ freshen() {
 	go get -u golang.org/x/tools/cmd/goimports
 	go get -u mvdan.cc/sh/cmd/shfmt
 	go get -u github.com/shurcooL/markdownfmt
-	go get -u github.com/govend/govend
-	go get -u github.com/alecthomas/gometalinter
-	gometalinter --install --update
 }
 
 if [[ "$#" -gt 0 ]]; then
